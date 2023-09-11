ce8946d5bbde06eb9e8fb6fa0f0a120c112923f3
luisburigo <guga.burigo29@outlook.com>
2023-09-11 19:44:38 -0300

    chore: debug env

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,16 @@ class App {
 
   static async connectDatabase() {
     try {
+    console.log({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [__dirname + '/entities/**/*.ts'],
+        synchronize: false,
+    })
       await createConnection({
         type: 'postgres',
         host: process.env.DATABASE_HOST,
