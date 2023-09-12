a03284086d4c218e3cdd8690d8bc3324294bfc4b
Pedro Barão <phbarao@gmail.com>
2023-09-12 15:02:31 -0300

    feat: add transactions migration

---
+++ src/database/migrations/1694536141266-create-table-transactions.ts
@@ -0,0 +1,92 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTableTransactions1694536141266 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'predicateAdress',
+            type: 'varchar',
+          },
+          {
+            name: 'predicateID',
+            type: 'integer',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'assets',
+            type: 'text',
+          },
+          {
+            name: 'witnesses',
+            type: 'text',
+          },
+          {
+            name: 'txData',
+            type: 'varchar',
+          },
+          {
+            name: 'hash',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'sendTime',
+            type: 'varchar',
+          },
+          {
+            name: 'gasUsed',
+            type: 'varchar',
+          },
+          {
+            name: 'resume',
+            type: 'text',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FK-predicate-transactions',
+            columnNames: ['predicateID'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'predicates',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
