d6472cf96a0b15f143e21f6ceea30812be724496
Pedro Barão <phbarao@gmail.com>
2023-09-19 11:51:51 -0300

    feat: add assets module

---
+++ src/modules/asset/types.ts
@@ -0,0 +1,22 @@
+import { Asset } from '@src/models';
+
+export interface ICreateAssetPayload {
+  assetId: string;
+  to: string;
+  amount: number;
+  transactionID: string;
+}
+
+export interface IUpdateAssetPayload {
+  assetId?: string;
+  to?: string;
+  amount?: number;
+  transactionID?: string;
+}
+
+export interface IAssetService {
+  create: (payload: ICreateAssetPayload) => Promise<Asset>;
+  update: (id: string, payload: IUpdateAssetPayload) => Promise<Asset>;
+  findById: (id: string) => Promise<Asset>;
+  list: () => Promise<Asset[]>;
+}
