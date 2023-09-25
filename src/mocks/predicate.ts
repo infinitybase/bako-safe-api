2d40994d324d10d87b55d03c63662af41f026f89
danmtsu <51559686+danmtsu@users.noreply.github.com>
2023-09-25 09:34:33 -0300

    atualizando
    atualizando

--- src/mocks/predicate.ts
+++ src/mocks/predicate.ts
@@ -17,3 +17,77 @@ export const predicate: IPredicatePayload = {
   provider: 'network',
   chainId: 1,
 };
+
+export const predicateNegativeMinSigners: IPredicatePayload = {
+  name: '123',
+  predicateAddress:
+    '0x0000000000000000000000000000000000000000000000000000000000000001',
+  description: 'desc_test',
+  minSigners: -1,
+  addresses: [
+    '0x0000000000000000000000000000000000000000000000000000000000000002',
+    '0x0000000000000000000000000000000000000000000000000000000000000003',
+  ],
+  owner: '0x0000000000000000000000000000000000000000000000000000000000007654',
+  bytes: 'Bytes*#*@&@',
+  abi: 'abi#&@&@*',
+  configurable: 'configurable',
+  provider: 'network',
+  chainId: 12,
+};
+export const predicateNullAddress: IPredicatePayload = {
+  name: 'Alberto Felchado',
+  predicateAddress:
+    '0x0000000000000000000000000000000000000000000000000000000000000001',
+  description: 'desc_test',
+  minSigners: -1,
+  addresses: [null],
+  owner: '0x0000000000000000000000000000000000000000000000000000000000007654',
+  bytes: 'Bytes*#*@&@',
+  abi: 'abi#&@&@*',
+  configurable: 'configurable',
+  provider: 'network',
+  chainId: 12,
+};
+export const predicateMinSignersNotInter: IPredicatePayload = {
+  name: 'Roy Mustang',
+  predicateAddress:
+    '0x0000000000000000000000000000000000000000000000000000000000000001',
+  description: 'desc_test',
+  minSigners: 2.5,
+  addresses: [],
+  owner: '0x0000000000000000000000000000000000000000000000000000000000007654',
+  bytes: 'Bytes*#*@&@',
+  abi: 'abi#&@&@*',
+  configurable: 'configurable',
+  provider: 'network',
+  chainId: 12,
+};
+export const predicateForUpdate: IPredicatePayload = {
+  name: 'Roy Mustang',
+  predicateAddress:
+    '0x0000000000000000000000000000000000000000000000000000000000000123',
+  description: 'desc_test',
+  minSigners: 2,
+  addresses: [],
+  owner: '0x0000000000000000000000000000000000000000000000000000000000042069',
+  bytes: 'Bytes*#*@&@',
+  abi: 'abi#&@&@*',
+  configurable: 'configurabled',
+  provider: 'network',
+  chainId: 12,
+};
+export const wrongPredicate: IPredicatePayload = {
+  name: 'Apagado',
+  predicateAddress:
+    '0x0000000000000000000000000000000000000000000000000000000000000001',
+  description: 'desc_test',
+  minSigners: -1,
+  addresses: [],
+  owner: '0x0000000000000000000000000000000000000000000000000000000000007777',
+  bytes: 'Bytes*#*@&@',
+  abi: 'abi#&@&@*',
+  configurable: 'configurable',
+  provider: 'network',
+  chainId: -512312,
+};
