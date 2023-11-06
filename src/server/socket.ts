15f64bfe99d163f73ef5eea8ec79eb65e3ea8180
guimroque <guilhermemr_2017@hotmail.com>
2023-11-06 08:55:24 -0300

    feat(socket): create a defult channels and middleware to connection

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -5,6 +5,18 @@ export interface ISocketUser {
   username: string;
 }
 
+export enum SocketChannels {
+  WALLET = '[WALLET]',
+  POPUP_AUTH = '[POPUP_AUTH]',
+  POPUP_TRANSFER = '[POPUP_TRANSFER]',
+}
+
+export interface ISocketEvent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  content: any; // todo: typing all events, and useing or
+  to: string;
+}
+
 class SocketIOServer extends Server {
   //todo: upgrade this type
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,36 +37,63 @@ class SocketIOServer extends Server {
     //middleware de conexao
     this.io.use((socket, next) => {
       const username = socket.handshake.auth.username;
-      if (!username) {
+      const hasValidEvent =
+        username.includes(SocketChannels.WALLET) ||
+        username.includes(SocketChannels.POPUP_AUTH) ||
+        username.includes(SocketChannels.POPUP_TRANSFER);
+      if (!username || !hasValidEvent) {
         return next(new Error('invalid username'));
       }
-      socket.emit('user', username);
+
       console.log('[user_connectted]:', username);
       socket.username = username;
       next();
     });
 
     this.io.on('connection', socket => {
-      const users: ISocketUser[] = [];
-      for (const [id, socket] of this.io.of('/').sockets) {
-        users.push({ userID: id, username: socket.username });
-      }
-      //socket.emit('users', users);
-      socket.on('[WALLET]', ({ content, to }) => {
-        console.log('[WALLET], content, to', content, to);
-        socket.to(to).emit('[WALLET`]', {
+      //[to list all users]
+      // const users: ISocketUser[] = [];
+      // for (const [id, socket] of this.io.of('/').sockets) {
+      //   users.push({ userID: id, username: socket.username });
+      // }
+
+      /* 
+        [WALLET]
+        - complement this connection depends to event content
+        - for exemple, complement payload to message to send to client
+      */
+      socket.on('[WALLET]', ({ content, to }: ISocketEvent) => {
+        console.log('[WALLET]');
+        socket.to(to).emit('[WALLET]', {
           content,
           from: socket.id,
         });
       });
 
-      socket.broadcast.emit('user connected', {
-        userID: socket.id,
-        username: socket.username,
+      /* 
+        [POPUP_TRANSFER]
+        - complement this connection depends to event content
+        - for exemple, complement payload to message to send to client
+      */
+      socket.on('[POPUP_TRANSFER]', ({ content, to }: ISocketEvent) => {
+        console.log('[POPUP_TRANSFER]');
+        socket.to(to).emit('[POPUP_TRANSFER]', {
+          content,
+          from: socket.id,
+        });
       });
 
-      socket.on('disconnect', () => {
-        socket.broadcast.emit('user disconnected', socket.id);
+      /* 
+        [POPUP_AUTH]
+        - complement this connection depends to event content
+        - for exemple, complement payload to message to send to client
+      */
+      socket.on('[POPUP_AUTH]', ({ content, to }: ISocketEvent) => {
+        console.log('[POPUP_AUTH]');
+        socket.to(to).emit('[POPUP_AUTH]', {
+          content,
+          from: socket.id,
+        });
       });
     });
   }
