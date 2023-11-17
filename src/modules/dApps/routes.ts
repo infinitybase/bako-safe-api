5dc29d5a75b3327285281bb46b885c8d300fa7eb
Pedro Barão <phbarao@gmail.com>
2023-11-17 18:27:16 -0300

    feat(wallet-connector): get current vault

--- src/modules/dApps/routes.ts
+++ src/modules/dApps/routes.ts
@@ -7,10 +7,13 @@ import { DAppsService } from './service';
 
 const router = Router();
 const dAppService = new DAppsService();
-const { currentAccount, accounts, state } = new DappController(dAppService);
+const { currentAccount, current, accounts, state } = new DappController(
+  dAppService,
+);
 
 router.get('/:sessionId/state', handleResponse(state));
 router.get('/:sessionId/accounts', handleResponse(accounts));
 router.get('/:sessionId/currentAccount', handleResponse(currentAccount));
+router.get('/:sessionId', handleResponse(current));
 
 export default router;
