093de649ad5cabd9a8b739909da81f196022455f
guimroque <guilhermemr_2017@hotmail.com>
2024-02-02 19:03:05 -0300

    fix(vatul_template): returns on vault template

--- src/database/seeders/120920231842-create-vault-template.ts
+++ src/database/seeders/120920231842-create-vault-template.ts
@@ -1,6 +1,6 @@
 import { generateInitialTemplate } from '@mocks/initialSeeds';
 
-import VaultTemplate from '@src/models/VaultTemplate';
+import { VaultTemplate } from '@src/models/VaultTemplate';
 
 export default async function () {
   await VaultTemplate.create(await generateInitialTemplate()).save();
