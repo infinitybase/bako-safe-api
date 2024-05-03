6fcfe2f104f6c50aae836cb74755f622bf27ffa9
guimroque <guilhermemr_2017@hotmail.com>
2024-05-03 19:35:22 -0300

    wip: intercept request and log infos

--- packages/geteway/src/index.ts
+++ packages/geteway/src/index.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import express from 'express'
 import { buildHTTPExecutor } from '@graphql-tools/executor-http'
 import { createHandler } from 'graphql-http/lib/use/express'
-import expressPlayground from 'graphql-playground-middleware-express'
 import { schemaFromExecutor } from '@graphql-tools/wrap'
 
 const { PROVIDER } = process.env
@@ -10,42 +10,60 @@ async function main() {
 	const app = express()
 	app.use(express.json())
 
-	// Middleware para logar as requisições
-	const requestLoggingMiddleware = (req, res, next) => {
-		const requestDetails = {
-			method: req.method,
-			url: req.url,
-			headers: req.headers,
-			body: req.body,
-		}
-		console.info('Solicitação recebida:', requestDetails)
-		next()
-	}
-
-	// Configuração do executor HTTP para o GraphQL
 	const executor = buildHTTPExecutor({
 		endpoint: PROVIDER, // Certifique-se que PROVIDER é uma URL completa
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
 	})
 
-	// Criar o schema utilizando o executor
 	const schema = await schemaFromExecutor(executor)
+	const handler = createHandler({
+		schema,
+		//@ts-ignore
+		execute({ schema, operationName, document, variableValues, ...rest }) {
+			console.log('Executando a operação:', {
+				operationName,
+				// 	document,
+				variableValues,
+			})
+			return executor({
+				document,
+				operationName,
+				variables: variableValues,
+				...rest,
+			})
+		},
+	}) // Crie o handler uma única vez
 
-	// Aplicar o middleware de log antes das rotas
-	app.use(requestLoggingMiddleware)
+	app.all('/graphql', async (req, res) => {
+		try {
+			// console.log(await parseRequestParams(req, res))
+			return handler(req, res, err => {
+				if (err) {
+					console.error('Erro ao processar a requisição:', err)
+					res.status(500).json({ error: 'Erro interno' })
+				}
+				console.log('Requisição processada com sucesso:', res.statusCode, res.statusMessage, res.body)
+			})
+		} catch (e) {
+			console.error('Erro ao processar a requisição:', e)
+			res.status(500).json({ error: 'Erro interno' })
+		}
+	}) // Use o handler para todas as requisições /graphql
 
-	app.get(
-		'/graphql',
-		expressPlayground({
-			endpoint: '/graphql',
-			settings: {
-				'schema.polling.enable': false,
-			},
-		}),
-	)
+	// app.get(
+	// 	'/graphql',
+	// 	expressPlayground({
+	// 		endpoint: '/graphql',
+	// 		settings: {
+	// 			'schema.polling.enable': true,
+	// 		},
+	// 	}),
+	// )
 
-	app.post('/graphql', createHandler({ schema }))
-
-	// Iniciar o servidor Express na porta 4001
 	app.listen(4001, () => {
 		console.log(`Servidor rodando em http://localhost:4001`)
 	})
