b3d195768f07f7f605ca5b88a208171fea9634a5
pedroanastacio <pedro.anastacio013@gmail.com>
2024-06-18 19:02:41 -0300

    fix(user): add duplicated name validation in update endpoint

--- packages/api/src/modules/user/controller.ts
+++ packages/api/src/modules/user/controller.ts
@@ -1,7 +1,7 @@
 import { addMinutes } from 'date-fns';
 
 import { RecoverCode, RecoverCodeType } from '@src/models';
-import { User } from '@src/models/User';
+import { TypeUser, User } from '@src/models/User';
 import { bindMethods } from '@src/utils/bindMethods';
 
 import {
@@ -29,6 +29,7 @@ import {
   IUpdateRequest,
   IUserService,
 } from './types';
+import { Not } from 'typeorm';
 
 export class UserController {
   private userService: IUserService;
@@ -219,6 +220,7 @@ export class UserController {
     try {
       const { user } = req;
       const { id } = req.params;
+      const { name } = req.body;
 
       if (user.id !== id) {
         throw new Unauthorized({
@@ -226,6 +228,20 @@ export class UserController {
           title: UnauthorizedErrorTitles.INVALID_PERMISSION,
           detail: `User id ${user.id} is not allowed to update user id ${id}`,
         });
+      }
+
+      if (name && user.type === TypeUser.WEB_AUTHN) {
+        const existingUser = await User.findOne({
+          where: { name, id: Not(user.id) },
+        });
+
+        if (existingUser) {
+          throw new BadRequest({
+            type: ErrorTypes.Update,
+            title: 'Error on user update',
+            detail: `User with name ${name} already exists`,
+          });
+        }
       }
 
       const response = await this.userService.update(id, {
