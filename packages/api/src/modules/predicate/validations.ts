af7b0cb4aac601cf41c091d01a9d6b5caaa11811
luisburigo <guga.burigo29@outlook.com>
2024-08-07 17:38:26 -0300

    feat(b256-address): convert predicate address to b256 and validate the format in payload

--- packages/api/src/modules/predicate/validations.ts
+++ packages/api/src/modules/predicate/validations.ts
@@ -1,14 +1,39 @@
 import Joi from 'joi';
 
 import { validator } from '@utils/index';
+import { Address } from 'fuels';
 
 export const validateAddPredicatePayload = validator.body(
   Joi.object({
     name: Joi.string().required(),
     description: Joi.string().allow('').optional(),
-    predicateAddress: Joi.string().required(),
+    predicateAddress: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        try {
+          const address = Address.fromString(value);
+          return address.toB256();
+        } catch (error) {
+          return helpers.message({ custom: 'Invalid address' });
+        }
+      }),
     minSigners: Joi.number().strict(true).integer().min(1).required(),
-    addresses: Joi.array().items(Joi.string()).min(1).required(),
+    addresses: Joi.array()
+      .items(Joi.string())
+      .min(1)
+      .required()
+      .custom((value, helpers) => {
+        const addresses = value.map(address => {
+          try {
+            const addressInstance = Address.fromString(address);
+            return addressInstance.toB256();
+          } catch (error) {
+            return helpers.message({ custom: 'Invalid address' });
+          }
+        });
+
+        return addresses;
+      }),
     configurable: Joi.string().required(),
     provider: Joi.string().required(),
     chainId: Joi.number().strict(true),
