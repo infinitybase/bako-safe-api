33cf84435500259666d2a58257643d7bc9ebd07c
luisburigo <guga.burigo29@outlook.com>
2024-08-07 16:17:37 -0300

    feat(b256-address): fix predicate address migration

--- packages/api/src/database/migrations/1720477253102-change-address-type-predicate.ts
+++ packages/api/src/database/migrations/1720477253102-change-address-type-predicate.ts
@@ -1,24 +1,24 @@
-import { Address } from "fuels";
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { Address } from 'fuels';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ChangeAddressTypePredicate1720477253102 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const predicates = await queryRunner.query(
+      `SELECT * FROM predicates p WHERE p."predicateAddress" LIKE 'fuel%'`,
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        const predicates = await queryRunner.query(
-            `SELECT * FROM predicates WHERE predicateaddress LIKE 'fuel%'`
-        );
-
-        for (const predicate of predicates) {
-            const newAddress = Address.fromString(predicate.predicateaddress).toHexString();
-            await queryRunner.query(
-                `UPDATE predicates SET predicateaddress = $1 WHERE id = $2`,
-                [newAddress, predicate.id]
-            );
-        }
+    for (const predicate of predicates) {
+      const newAddress = Address.fromString(
+        predicate.predicateAddress,
+      ).toHexString();
+      await queryRunner.query(
+        `UPDATE predicates SET "predicateAddress" = $1 WHERE id = $2`,
+        [newAddress, predicate.id],
+      );
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        return;
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
 }
