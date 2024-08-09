90d1d0f9abeb7d55c814886cfa5bd0763b63579d
pedroanastacio <pedro.anastacio013@gmail.com>
2024-08-09 14:46:24 -0300

    feat(transactions): creates functions to handle fuel transactions

--- packages/api/src/modules/transaction/utils.ts
+++ packages/api/src/modules/transaction/utils.ts
@@ -7,6 +7,9 @@ import {
 } from './types';
 import { IDeposit } from '../predicate/types';
 import { TransactionStatus } from 'bakosafe';
+import { TransactionResult } from 'fuels';
+import { formatAssets } from '@src/utils/formatAssets';
+import { IOrdination } from '@src/utils/ordination/helper';
 
 export const formatTransactionsResponse = (
   transactions: IPagination<Transaction> | Transaction[],
@@ -117,4 +120,125 @@ export const formatPayloadToCreateTransaction = (
   };
 
   return payload;
+};
+
+export const formatFuelTransaction = (
+  tx: TransactionResult,
+  predicate: Predicate,
+): ITransactionResponse => {
+  const {
+    gasPrice,
+    scriptGasLimit,
+    script,
+    scriptData,
+    type,
+    witnesses,
+    outputs,
+    inputs,
+  } = tx.transaction;
+
+  const formattedTransaction = {
+    id: tx.id,
+    name: `DEPOSIT_${tx.id}`, // TODO: change this
+    hash: tx.id.slice(2),
+    sendTime: tx.date,
+    createdAt: tx.date,
+    updatedAt: tx.date,
+    type: TransactionType.DEPOSIT,
+    txData: {
+      gasPrice,
+      scriptGasLimit,
+      script,
+      scriptData,
+      type,
+      witnesses,
+      outputs,
+      inputs,
+    },
+    status: TransactionStatus.SUCCESS,
+    summary: null,
+    gasUsed: tx.gasUsed.format(),
+    resume: {
+      hash: tx.id,
+      status: TransactionStatus.SUCCESS,
+      witnesses: [],
+      requiredSigners: predicate.minSigners,
+      totalSigners: predicate.members.length,
+      predicate: {
+        id: predicate.id,
+        address: predicate.predicateAddress,
+      },
+      id: tx.id,
+    },
+    createdBy: predicate.owner,
+    predicateId: predicate.id,
+    predicate: {
+      id: predicate.id,
+      name: predicate.name,
+      minSigners: predicate.minSigners,
+      predicateAddress: predicate.predicateAddress,
+      members: predicate.members,
+      workspace: {
+        id: predicate.workspace.id,
+        name: predicate.workspace.name,
+        single: predicate.workspace.single,
+      },
+    },
+    assets: formatAssets(outputs),
+  };
+
+  return (formattedTransaction as unknown) as ITransactionResponse;
+};
+
+export const mergeTransactionLists = (
+  bakoList: IPagination<ITransactionResponse> | ITransactionResponse[],
+  fuelList: ITransactionResponse[],
+  ordination: IOrdination<Transaction>,
+  perPage?: number,
+): IPagination<ITransactionResponse> | ITransactionResponse[] => {
+  const isArray = Array.isArray(bakoList);
+  const bakoListArray: ITransactionResponse[] = isArray ? bakoList : bakoList.data;
+  const _bakoList = new Set(bakoListArray.map(tx => tx.hash));
+  const missingTxs = fuelList.filter(tx => !_bakoList.has(tx.hash));
+  const mergedList = [...bakoListArray, ...missingTxs];
+  const sortedList = sortTransactions(mergedList, ordination);
+  const list = perPage ? sortedList.slice(0, perPage) : sortedList;
+  const result = isArray ? list : { ...bakoList, data: list };
+
+  return result;
+};
+
+export const sortTransactions = (
+  transactions: ITransactionResponse[],
+  ordination: IOrdination<Transaction>,
+): ITransactionResponse[] => {
+  const { orderBy, sort } = ordination;
+
+  const sortedTransactions = transactions.sort((a, b) => {
+    let compareValue = 0;
+
+    const aValue = a[orderBy];
+    const bValue = b[orderBy];
+
+    switch (orderBy) {
+      case 'createdAt':
+      case 'updatedAt':
+      case 'sendTime':
+        compareValue =
+          new Date(aValue as string).getTime() -
+          new Date(bValue as string).getTime();
+        break;
+      default:
+        if (typeof aValue === 'string' && typeof bValue === 'string') {
+          compareValue = aValue.localeCompare(bValue);
+        } else {
+          compareValue = 0;
+        }
+        break;
+    }
+
+    return sort === 'ASC' ? compareValue : -compareValue;
+  });
+
+  return sortedTransactions;
 };
