9986264bf76c6036aba7a82c9c605304f2dc0b72
guimroque <guilhermemr_2017@hotmail.com>
2024-08-12 15:53:05 -0300

    fix: hotfix

--- packages/api/src/database/migrations/1722946398593-change-address-type-predicate.ts
+++ packages/api/src/database/migrations/1722946398593-change-address-type-predicate.ts
@@ -2,31 +2,30 @@ import { Address } from 'fuels';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ChangeAddressTypePredicate1722946398593 implements MigrationInterface {
-public async up(queryRunner: QueryRunner): Promise<void> {
-  const predicates = await queryRunner.query(
-    `SELECT * FROM predicates p WHERE p."predicateAddress" LIKE 'fuel%'`,
-  );
-
-  for (const predicate of predicates) {
-    const newAddress = Address.fromString(
-      predicate.predicateAddress,
-    ).toHexString();
-
-    // Verificar se o novo endereço já existe no banco de dados
-    const existingPredicate = await queryRunner.query(
-      `SELECT id FROM predicates WHERE p."predicateAddress" = $1`,
-      [newAddress],
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const predicates = await queryRunner.query(
+      `SELECT * FROM predicates p WHERE p."predicateAddress" LIKE 'fuel%'`,
     );
 
-    if (existingPredicate.length === 0) {
-      await queryRunner.query(
-        `UPDATE predicates SET predicateAddress = $1 WHERE id = $2`,
-        [newAddress, predicate.id],
+    for (const predicate of predicates) {
+      const newAddress = Address.fromString(
+        predicate.predicateAddress,
+      ).toHexString();
+
+      // Verificar se o novo endereço já existe no banco de dados
+      const existingPredicate = await queryRunner.query(
+        `SELECT id FROM predicates WHERE "predicateAddress" = $1`,
+        [newAddress],
       );
+
+      if (existingPredicate.length === 0) {
+        await queryRunner.query(
+          `UPDATE predicates SET "predicateAddress" = $1 WHERE id = $2`,
+          [newAddress, predicate.id],
+        );
+      }
     }
   }
-}
-
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     return;
