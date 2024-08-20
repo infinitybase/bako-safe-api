4d64f0969d62fc8a25dd62b8ad44af9ca970f924
guimroque <guilhermemr_2017@hotmail.com>
2024-08-20 12:46:50 -0300

    chore: debbug

--- packages/api/src/socket/client.ts
+++ packages/api/src/socket/client.ts
@@ -1,5 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { IMessage, SocketEvents, SocketUsernames } from './types';
+import { sha256 } from 'fuels';
 
 
 export class SocketClient {
@@ -21,6 +22,7 @@ export class SocketClient {
 
   // Método para enviar uma mensagem para o servidor
   sendMessage(message: IMessage) {
+    console.log('[EMITINDO MENSAGEM]: ', message, SocketEvents.DEFAULT);
     this.socket.emit(SocketEvents.DEFAULT, message);
   }
 
