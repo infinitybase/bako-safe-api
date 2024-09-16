c13c1a39689fabe5a20eebf9ff8bdf36942f48f8
Rafael <rafael-dev@hotmail.com>
2024-09-16 19:56:42 -0300

    fix(auth): adjust some props on qb predicate

--- packages/api/src/modules/auth/services.ts
+++ packages/api/src/modules/auth/services.ts
@@ -4,11 +4,7 @@ import { Predicate, User } from '@models/index';
 import { ErrorTypes } from '@utils/error/GeneralError';
 import Internal from '@utils/error/Internal';
 
-import {
-  IAuthService,
-  ICreateUserTokenPayload,
-  ISignInResponse,
-} from './types';
+import { IAuthService, ICreateUserTokenPayload, ISignInResponse } from './types';
 import { LessThanOrEqual } from 'typeorm';
 
 export class AuthService implements IAuthService {
@@ -60,20 +56,23 @@ export class AuthService implements IAuthService {
       .where('ut.token = :signature', { signature })
       .andWhere('ut.expired_at > :now', { now: new Date() });
 
-      const {user, workspace, ...token } = await QBtoken.getOne();
-      if(!token){
-        return undefined;
-      }
+    const { user, workspace, ...token } = await QBtoken.getOne();
+    if (!token) {
+      return undefined;
+    }
 
-      // console.log('[FIND_TOKEN_INFO]: ', { user, workspace, token });
-      const QBPredicate = Predicate.createQueryBuilder('p')
-        .innerJoin('p.owner', 'owner')
-        .select(['p.id',  'p.root', 'owner.id'])
-        .where('p.owner_id = :userId', { userId: workspace.id })
-        .where('p.root = :root ', { root: true });
+    // console.log('[FIND_TOKEN_INFO]: ', { user, workspace, token });
+    const QBPredicate = Predicate.createQueryBuilder('p')
+      .innerJoin('p.owner', 'owner')
+      .select(['p.id', 'p.root', 'owner.id'])
+      .where('p.owner_id = :userId', { userId: user.id })
+      .andWhere('p.root = :root ', { root: true });
 
-      const {id: predicateId} = await QBPredicate.getOne();
-      
+    const result = await QBPredicate.getOne();
+
+    console.log('result:', result);
+
+    if (!result) {
       return {
         //session
         accessToken: token.token,
@@ -84,13 +83,32 @@ export class AuthService implements IAuthService {
         user_id: user.id,
         avatar: user.avatar,
         address: user.address,
-        rootWallet: predicateId ?? 'not found',
+        rootWallet: 'not found',
         // workspace
         workspace: {
           ...workspace,
           permissions: workspace.permissions[user.id],
         },
       };
+    }
+
+    return {
+      //session
+      accessToken: token.token,
+      expired_at: token.expired_at,
+      // user
+      name: user.name,
+      type: user.type,
+      user_id: user.id,
+      avatar: user.avatar,
+      address: user.address,
+      rootWallet: result.id,
+      // workspace
+      workspace: {
+        ...workspace,
+        permissions: workspace.permissions[user.id],
+      },
+    };
   }
 
   static async clearExpiredTokens(): Promise<void> {
