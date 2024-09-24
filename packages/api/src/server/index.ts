7ef73ad81045b1d48b030e55c229c869d360351c
guimroque <guilhermemr_2017@hotmail.com>
2024-09-24 12:07:57 -0300

    chore: debbug

--- packages/api/src/server/index.ts
+++ packages/api/src/server/index.ts
@@ -12,7 +12,9 @@ const {
   FUEL_PROVIDER, 
   GAS_LIMIT, 
   MAX_FEE,
-  COIN_MARKET_CAP_API_KEY
+  COIN_MARKET_CAP_API_KEY,
+  AWS_SMTP_USER,
+  AWS_SMTP_PASS
 } = process.env;
 
 const start = async () => {
@@ -28,7 +30,9 @@ const start = async () => {
     FUEL_PROVIDER,
     GAS_LIMIT,
     MAX_FEE,
-    COIN_MARKET_CAP_API_KEY
+    COIN_MARKET_CAP_API_KEY,
+    AWS_SMTP_USER,
+    AWS_SMTP_PASS
   })
 
 
