ac48afb6b06d573839bdfd8c417ea0708c7b8406
guimroque <guilhermemr_2017@hotmail.com>
2024-10-03 23:20:35 -0300

    chore: force deploy

--- packages/api/src/database/migrations/1726496150864-predicate-root.ts
+++ packages/api/src/database/migrations/1726496150864-predicate-root.ts
@@ -1,18 +1,19 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class PredicateRoot1726496150864 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'predicates',
+      new TableColumn({
+        name: 'root',
+        type: 'boolean',
+        default: false,
+        isNullable: true,
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn('predicates', new TableColumn({
-            name: 'root',
-            type: 'boolean',
-            default: false, 
-            isNullable: true,
-        }));
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn('predicates', 'root');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('predicates', 'root');
+  }
 }
