2ccd0c53a70fc38889eea54cdc32b1261a2d6165
guimroque <guilhermemr_2017@hotmail.com>
2024-10-23 15:06:21 -0300

    chore: remove table roles

--- packages/api/src/database/seeders/110820231840-create-users.ts
+++ packages/api/src/database/seeders/110820231840-create-users.ts
@@ -1,13 +1,6 @@
 import { generateInitialUsers } from '@src/mocks/initialSeeds/initialUsers';
-import {
-  PermissionRoles,
-  Workspace,
-  defaultPermissions,
-} from '@src/models/Workspace';
-import { UserService } from '@src/modules/user/service';
 
-import { User } from '@models/index';
-import { IconUtils } from '@utils/icons';
+import { UserService } from '@src/modules/user/service';
 
 export default async function () {
   const users = await generateInitialUsers();
