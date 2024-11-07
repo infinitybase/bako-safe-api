d9ac71d78b71f7da43cfe640e5588d74f0007ca5
Mateus Paegle <mateus.martinegle@gmail.com>
2024-11-07 14:06:56 -0300

    fix: replace host by %

--- packages/api/src/database/migrations/1730994094877-add-metabase-db-user.ts
+++ packages/api/src/database/migrations/1730994094877-add-metabase-db-user.ts
@@ -5,11 +5,11 @@ const { DATABASE_NAME, DB_METABASE_USERNAME, DB_METABASE_PASS } = process.env;
 export class AddMetabaseDbUser1730994094877 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE USER '${DB_METABASE_USERNAME}'@'host' IDENTIFIED BY '${DB_METABASE_PASS}';`,
+      `CREATE USER '${DB_METABASE_USERNAME}'@'%' IDENTIFIED BY '${DB_METABASE_PASS}';`,
     );
 
     await queryRunner.query(
-      `GRANT SELECT ON ${DATABASE_NAME}.* TO '${DB_METABASE_USERNAME}'@'host';`,
+      `GRANT SELECT ON ${DATABASE_NAME}.* TO '${DB_METABASE_USERNAME}'@'%';`,
     );
 
     await queryRunner.query(`FLUSH PRIVILEGES;`);
