4eaf40a7e52fec8ec18bd17fd7fb33ee5a092e6c
guimroque <guilhermemr_2017@hotmail.com>
2025-01-08 13:52:48 -0300

    chore: asset currency description

--- packages/worker/src/queues/assetsValue/queue.ts
+++ packages/worker/src/queues/assetsValue/queue.ts
@@ -8,6 +8,18 @@ const assetQueue = new Queue(QUEUE_ASSET, {
   redis: redisConfig,
 });
 
+/**
+ * This queue is designed to keep the USD value (in USDC) of specific assets updated.
+ * The update frequency depends on the cron expression, with a recommended interval of 20 minutes.
+ *
+ * Process:
+ * 1. Fetch the list of verified assets from the Fuel Network using the link: https://verified-assets.fuel.network/assets.json.
+ * 2. Filter the assets to include only those on the Fuel Network (mainnet and testnet).
+ * 3. Map the filtered assets to match the naming conventions used by the CoinGecko API.
+ * 4. Retrieve the USD values of the assets from the CoinGecko API. link: https://api.coingecko.com/api/v3/simple/
+ * 5. Update the database with the fetched USD values, using USDC as the base currency.
+ */
+
 assetQueue.process(async (job) => {
   const db = await MongoDatabase.connect();
   const collection = db.getCollection(CollectionName.ASSET_BALANCE);
