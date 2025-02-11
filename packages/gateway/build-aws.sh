6d9e4e994912a73564ded2aa44dedd428a68421f
jonatanbolsoni <jonatan.bolsoni@gmail.com>
2025-02-11 12:09:30 -0300

    remove .sh

--- packages/gateway/build-aws.sh
+++
@@ -1,2 +0,0 @@
-aws ecr get-login-password --region us-east-1 --profile infinitybase-jota | docker login --username AWS --password-stdin 392170836981.dkr.ecr.us-east-1.amazonaws.com
-docker buildx build --platform linux/arm64 --push -t 392170836981.dkr.ecr.us-east-1.amazonaws.com/bako-safe-gateway-api-hmg:v1 .
