8c3ea9a9687cc0b64877ec4120db9784a5df6b45
vitor <pereiravitor1218@gmail.com>
2025-08-12 20:03:43 -0300

    feat(utils): add TRANSAK to serviceProvidersWithFuelIntegration

--- packages/api/src/modules/meld/utils.ts
+++ packages/api/src/modules/meld/utils.ts
@@ -19,7 +19,7 @@ export const meldApi = axios.create({
   },
 });
 
-export const serviceProvidersWithFuelIntegration = ['BANXA'];
+export const serviceProvidersWithFuelIntegration = ['BANXA', 'TRANSAK'];
 export const FIAT_CURRENCIES = ['BRL', 'USD', 'EUR'];
 export const CRYPTO_CURRENCIES = ['ETH_FUEL'];
 
