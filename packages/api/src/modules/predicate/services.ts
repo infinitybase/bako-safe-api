7073dd8ed5413d46bcb529188c850550cb99e0df
pedroanastacio <pedro.anastacio013@gmail.com>
2025-11-04 11:42:54 -0300

    fix(predicate): create predicates based on versions

--- packages/api/src/modules/predicate/services.ts
+++ packages/api/src/modules/predicate/services.ts
@@ -449,34 +449,11 @@ export class PredicateService implements IPredicateService {
   async checkOlderPredicateVersions(
     address: string,
     provider: string,
-  ): Promise<{ invisibleAccounts: string[]; accounts: Vault[] }> {
+  ): Promise<Vault[]> {
     const legacyVersions = await legacyConnectorVersion(address, provider);
 
-    const withBalance = legacyVersions
-      .filter(v => v.hasBalance)
-      .sort((a, b) => b.details.versionTime - a.details.versionTime);
-    const invisibleAccounts = legacyVersions
-      .filter(v => !v.hasBalance)
-      .map(v => v.predicateAddress);
-
-    if (withBalance.length === 0) {
-      const latest = getLatestPredicateVersion(WalletType.FUEL);
-      const config = { SIGNERS: [address], SIGNATURES_COUNT: 1 };
-
-      const latestVault = await this.instancePredicate(
-        JSON.stringify(config),
-        provider,
-        latest.version,
-      );
-
-      return {
-        invisibleAccounts,
-        accounts: [latestVault],
-      };
-    }
-
     const accounts = await Promise.all(
-      withBalance.map(async v => {
+      legacyVersions.map(async v => {
         const isFromConnector =
           v.details.origin === WalletType.EVM ||
           v.details.origin === WalletType.SVM;
@@ -489,7 +466,7 @@ export class PredicateService implements IPredicateService {
       }),
     );
 
-    return { invisibleAccounts, accounts };
+    return accounts;
   }
 
   async instancePredicate(
--- packages/api/src/modules/user/service.ts
+++ packages/api/src/modules/user/service.ts
@@ -124,35 +124,29 @@ export class UserService implements IUserService {
         // insert a root wallet predicate
         const provider = await FuelProvider.create(payload.provider);
 
-        const { accounts: wallets, invisibleAccounts } =
-          await new PredicateService().checkOlderPredicateVersions(
-            user.address,
-            provider.url,
-          );
+        const accounts = await new PredicateService().checkOlderPredicateVersions(
+          user.address,
+          provider.url,
+        );
 
-        const network: Network = {
-          url: provider.url,
-          chainId: await provider.getChainId(),
-        };
-
-        for (const [i, wallet] of wallets.entries()) {
+        for (const [i, account] of accounts.entries()) {
           const isFirst = i === 0;
           await Predicate.create({
             name: `${isFirst ? 'Personal Vault' : `Vault ${i + 1}`}`,
-            description: `${isFirst &&
+            description: `${
+              isFirst &&
               'This is your first vault. It requires a single signer (you) to execute transactions; a pattern called 1-of-1'
-              }`,
-            predicateAddress: new Address(wallet.address).toB256(),
-            configurable: JSON.stringify(wallet.configurable),
+            }`,
+            predicateAddress: new Address(account.address).toB256(),
+            configurable: JSON.stringify(account.configurable),
             root: isFirst,
-            version: wallet.version,
+            version: account.version,
             owner: user,
             workspace,
             members: [user],
           }).save();
         }
 
-        user.settings.inactivesPredicates.push(...invisibleAccounts);
         await user.save();
         return user;
       })
